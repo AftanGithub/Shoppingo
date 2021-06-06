71b1ba8bd7e144c19c03713653494587ed109ffa
Aftan <aftanahmadskills@gmail.com>
2021-06-06 11:32:18 +0530

    Loading logo added

--- ecommerce-site/src/App.js
+++ ecommerce-site/src/App.js
@@ -1,38 +1,95 @@
-import React, { useEffect } from 'react';
-import {Switch, Route} from 'react-router-dom';
-import {ToastContainer} from 'react-toastify';
+import React, { useEffect, lazy, Suspense } from "react";
+import { Switch, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import Login from './pages/auth/Login';
-import Register from './pages/auth/Register';
-import Home from './pages/Home';
-import Header from './components/nav/Header';
-import RegisterComplete from './pages/auth/RegisterComplete';
-import {auth} from './firebase';
-import {useDispatch} from 'react-redux';
-import ForgotPassword from './pages/auth/ForgotPassword';
-import {currentUser} from './functions/auth';
-import History from './pages/user/History';
-import UserRoute from './components/Routes/UserRoute';
-import AdminRoute from './components/Routes/AdminRoute';
-import Password from './pages/user/Password';
-import Wishlist from './pages/user/Wishlist';
-import AdminDashboard from './pages/admin/AdminDashboard';
-import CategoryCreate from "./pages/admin/category/CategoryCreate";
-import CategoryUpdate from "./pages/admin/category/CategoryUpdate";
-import SubCreate from "./pages/admin/sub/SubCreate";
-import SubUpdate from "./pages/admin/sub/SubUpdate";
-import ProductCreate from "./pages/admin/product/ProductCreate";
-import ProductUpdate from "./pages/admin/product/ProductUpdate";
-import AllProducts from './pages/admin/product/AllProducts';
-import Product from './pages/Product';
-import CategoryHome from "./pages/category/CategoryHome";
-import SubHome from "./pages/sub/SubHome";
-import Shop from './pages/Shop';
-import Cart from "./pages/Cart";
-import SideDrawer from "./components/drawer/SideDrawer";
-import Checkout from "./pages/Checkout";
-import Payment from "./pages/Payment";
-import CreateCouponPage from "./pages/admin/coupon/CreateCouponPage";
+
+import { auth } from "./firebase";
+import { useDispatch } from "react-redux";
+import { currentUser } from "./functions/auth";
+import { LoadingOutlined } from "@ant-design/icons";
+import logo from "./logo.png";
+
+
+// using lazy
+const Login = lazy(() => import("./pages/auth/Login"));
+const Register = lazy(() => import("./pages/auth/Register"));
+const Home = lazy(() => import("./pages/Home"));
+const Header = lazy(() => import("./components/nav/Header"));
+const SideDrawer = lazy(() => import("./components/drawer/SideDrawer"));
+
+const RegisterComplete = lazy(() => import("./pages/auth/RegisterComplete"));
+const ForgotPassword = lazy(() => import("./pages/auth/ForgotPassword"));
+const History = lazy(() => import("./pages/user/History"));
+const UserRoute = lazy(() => import("./components/Routes/UserRoute"));
+const AdminRoute = lazy(() => import("./components/Routes/AdminRoute"));
+const Password = lazy(() => import("./pages/user/Password"));
+const Wishlist = lazy(() => import("./pages/user/Wishlist"));
+const AdminDashboard = lazy(() => import("./pages/admin/AdminDashboard"));
+const CategoryCreate = lazy(() =>
+  import("./pages/admin/category/CategoryCreate")
+);
+const CategoryUpdate = lazy(() =>
+  import("./pages/admin/category/CategoryUpdate")
+);
+const SubCreate = lazy(() => import("./pages/admin/sub/SubCreate"));
+const SubUpdate = lazy(() => import("./pages/admin/sub/SubUpdate"));
+const ProductCreate = lazy(() => import("./pages/admin/product/ProductCreate"));
+const AllProducts = lazy(() => import("./pages/admin/product/AllProducts"));
+const ProductUpdate = lazy(() => import("./pages/admin/product/ProductUpdate"));
+const Product = lazy(() => import("./pages/Product"));
+const CategoryHome = lazy(() => import("./pages/category/CategoryHome"));
+const SubHome = lazy(() => import("./pages/sub/SubHome"));
+const Shop = lazy(() => import("./pages/Shop"));
+const Cart = lazy(() => import("./pages/Cart"));
+const Checkout = lazy(() => import("./pages/Checkout"));
+const CreateCouponPage = lazy(() =>
+  import("./pages/admin/coupon/CreateCouponPage")
+);
+const Payment = lazy(() => import("./pages/Payment"));
+
+
+
+
+
+
+
+
+
+// import React, { useEffect } from 'react';
+// import {Switch, Route} from 'react-router-dom';
+// import {ToastContainer} from 'react-toastify';
+// import "react-toastify/dist/ReactToastify.css";
+// import Login from './pages/auth/Login';
+// import Register from './pages/auth/Register';
+// import Home from './pages/Home';
+// import Header from './components/nav/Header';
+// import RegisterComplete from './pages/auth/RegisterComplete';
+// import {auth} from './firebase';
+// import {useDispatch} from 'react-redux';
+// import ForgotPassword from './pages/auth/ForgotPassword';
+// import {currentUser} from './functions/auth';
+// import History from './pages/user/History';
+// import UserRoute from './components/Routes/UserRoute';
+// import AdminRoute from './components/Routes/AdminRoute';
+// import Password from './pages/user/Password';
+// import Wishlist from './pages/user/Wishlist';
+// import AdminDashboard from './pages/admin/AdminDashboard';
+// import CategoryCreate from "./pages/admin/category/CategoryCreate";
+// import CategoryUpdate from "./pages/admin/category/CategoryUpdate";
+// import SubCreate from "./pages/admin/sub/SubCreate";
+// import SubUpdate from "./pages/admin/sub/SubUpdate";
+// import ProductCreate from "./pages/admin/product/ProductCreate";
+// import ProductUpdate from "./pages/admin/product/ProductUpdate";
+// import AllProducts from './pages/admin/product/AllProducts';
+// import Product from './pages/Product';
+// import CategoryHome from "./pages/category/CategoryHome";
+// import SubHome from "./pages/sub/SubHome";
+// import Shop from './pages/Shop';
+// import Cart from "./pages/Cart";
+// import SideDrawer from "./components/drawer/SideDrawer";
+// import Checkout from "./pages/Checkout";
+// import Payment from "./pages/Payment";
+// import CreateCouponPage from "./pages/admin/coupon/CreateCouponPage";
 
 const App=()=> {
   const dispatch = useDispatch();
@@ -65,7 +122,15 @@ useEffect(() => {
 }, [dispatch]);
 
   return (
-   <>
+    <Suspense
+      fallback={
+        <div className="col mt-5 text-center p-5">
+          <img src={logo}/>
+          <LoadingOutlined />
+         
+        </div>
+      }
+    >
     <Header/>
     <ToastContainer />
     <SideDrawer />
@@ -95,7 +160,7 @@ useEffect(() => {
      <UserRoute exact path="/payment" component={Payment} />
      <AdminRoute exact path="/admin/coupon" component={CreateCouponPage} />
    </Switch>
-   </>
+   </Suspense>
   );
 };
 
